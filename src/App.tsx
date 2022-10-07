cdb3e4046c6d35f36ff50d1d49fd1e5d8c0f3cd1
krit <krittanon@amity.co>
2022-10-07 15:57:14 +0700

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./styles.css";
-import { useMemo, useState } from "react";
-
-
+import { HashRouter, Route, Routes, useSearchParams } from "react-router-dom";
 import {
   AmityUiKitProvider,
   AmityUiKitSocial,
@@ -12,48 +10,37 @@ import {
 import Login from "./Login";
 import { NotiTray } from "./pages/NotiTray";
 
-const initState = () => {
-  const url = window.location.href;
-  const params = url.split("?")[1];
-  const urlParams = new URLSearchParams(params);
-  const page = urlParams.get("page") ?? "social";
-  const id = urlParams.get("id") ?? "";
-  const region = urlParams.get("region") ?? "";
-  const network = urlParams.get("network") ?? "";
-  const authToken = urlParams.get("authToken") ?? undefined;
-  return { id, region, network, authToken, page };
+const MainApp = () => {
+  const [searchParams] = useSearchParams();
+  const network = searchParams.get("network");
+  const userId = searchParams.get("userId");
+  const region = searchParams.get("region");
+  const authToken = searchParams.get("authToken") ?? undefined;
+  return (
+    <AmityUiKitProvider
+      apiRegion={region}
+      apiKey={network}
+      userId={userId}
+      authToken={authToken}
+    >
+      <Routes>
+        <Route path="chat" element={<AmityUiKitChat />} />
+        <Route path="social" element={<AmityUiKitSocial />} />
+        <Route path="notitray" element={<NotiTray />} />
+      </Routes>
+    </AmityUiKitProvider>
+  );
 };
 
 export default function App() {
-  const [state, setState] = useState<{
-    id: string;
-    network: string;
-    region: string;
-    authToken?: string;
-    page?: string;
-  }>(initState);
-
-  const renderpage = useMemo(() => {
-    let renderpage = <AmityUiKitSocial />;
-    if (state.page === "chat") renderpage = <AmityUiKitChat />;
-    if (state.page === "notitray") renderpage = <NotiTray />;
-    return renderpage;
-  }, [state.page]);
-
   return (
     <div className="App">
-      {!state.id ? (
-        <Login onSubmit={setState} />
-      ) : (
-        <AmityUiKitProvider
-          apiRegion={state.region}
-          apiKey={state.network}
-          userId={state.id}
-          authToken={state.authToken}
-        >
-          {renderpage}
-        </AmityUiKitProvider>
-      )}
+      <HashRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<MainApp />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
